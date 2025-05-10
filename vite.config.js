8525f2601b5f963ef8ddb045b0d04e0be518c301
Giovanni Morelli <elimarcleo@gmail.com>
2025-05-10 18:43:50 +0200

    Aggiunti nuovi aggiornamenti

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './build'  // Configura la directory di output per il build
+  }
 })
