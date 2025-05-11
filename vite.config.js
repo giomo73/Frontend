0449d0dc766d79b3d46c2c04df5367a9a3392ba1
Giovanni Morelli <elimarcleo@gmail.com>
2025-05-11 18:18:21 +0200

    Fix vite execution for Render

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './Dist'  // Configura la directory di output per il build
+    outDir: 'dist' // Output corretto (minuscolo)
   }
-})
+});
